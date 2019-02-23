4b415c1331baeff3566a657970099bc9691223df
Austin Blandford <ABlandford@student.neumont.edu>
2019-02-22 18:18:02 -0700

    Lotsa Fonts
    Added a ton of fonts and some more styling to the html.

--- GameBlogApplication/GameBlogApplication/Controllers/HomeController.cs
+++ GameBlogApplication/GameBlogApplication/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace GameBlogApplication.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Aaaaaaaaaall about us!!!";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Um..about contacting us...";
 
             return View();
         }
